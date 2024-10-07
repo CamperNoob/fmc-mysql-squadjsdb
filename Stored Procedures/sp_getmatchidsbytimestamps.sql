461c3d813e116110d54736d92d69d1ccb2265199
Misha Yudin <misha.yudin19@gmail.com>
2024-10-07 13:34:18 +0300

    Fixed sp_getmatchidsbytimestamps to create correct temp tables

--- Stored Procedures/sp_getmatchidsbytimestamps.sql
+++ Stored Procedures/sp_getmatchidsbytimestamps.sql
@@ -2,17 +2,17 @@ drop procedure if exists `sp_getmatchidsbytimestamps`;
 create procedure `sp_getmatchidsbytimestamps` (timestampsJson TEXT)
 begin
 	drop temporary table if exists `getmatchidsbytimestamps`;
-	drop temporary table if exists `getmatchidsbymap_timestampList`;
-	create temporary table `getmatchidsbymap` (matchId int);
-	create temporary table `getmatchidsbymap_timestampList` (searchTime datetime);
+	drop temporary table if exists `getmatchidsbytimestamps_timestampList`;
+	create temporary table `getmatchidsbytimestamps` (matchId int);
+	create temporary table `getmatchidsbytimestamps_timestampList` (searchTime datetime);
 
-	insert into getmatchidsbymap_timestampList (searchTime)
+	insert into getmatchidsbytimestamps_timestampList (searchTime)
 	select STR_TO_DATE(value, '%Y-%m-%d %H:%i') as `searchTime`
-	from JSON_TABLE(mapNameJson, '$[*]' columns(value varchar(255) path '$')) as jt;
+	from JSON_TABLE(timestampsJson, '$[*]' columns(value varchar(255) path '$')) as jt;
 
-	insert into getmatchidsbymap (matchID)
+	insert into getmatchidsbytimestamps (matchID)
 	select distinct a.id
 	from dblog_matches as a
-	right join getmatchidsbymap_timestampList as b on a.startTime between date_add(b.searchTime, interval -1 minute) and date_add(b.searchTime, interval 1 minute);
-	drop temporary table if exists `getmatchidsbymap_timestampList`;
+	right join getmatchidsbytimestamps_timestampList as b on a.startTime between date_add(date_add(b.searchTime, interval -3 hour), interval -1 minute) and date_add(date_add(b.searchTime, interval -3 hour), interval 1 minute);
+	drop temporary table if exists `getmatchidsbytimestamps_timestampList`;
 end
