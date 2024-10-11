9d199f542699aef7150d373532fc5b48b6bbd589
Misha Yudin <misha.yudin19@gmail.com>
2024-10-12 00:11:26 +0300

    Update trigger_dblogmatchesunignorelegit.sql

--- Triggers/trigger_dblogmatchesunignorelegit.sql
+++ Triggers/trigger_dblogmatchesunignorelegit.sql
@@ -3,7 +3,9 @@ create trigger `squadjs`.`trigger_dblogmatchesunignorelegit`
 before update on `dblog_matches`
 for each ROW
 BEGIN
-    IF (NEW.`ignore` = 1 and NEW.winner is not null and timestampdiff(minute, NEW.startTime, NEW.endTime) >= 15 and NEW.layerClassName not like '%_Seed%')
+    IF (NEW.`ignore` = 1 
+    --and NEW.winner is not null 
+    and timestampdiff(minute, NEW.startTime, NEW.endTime) >= 15 and NEW.layerClassName not like '%_Seed%')
 	    THEN SET NEW.`ignore` = 0;
     END IF;
 END
