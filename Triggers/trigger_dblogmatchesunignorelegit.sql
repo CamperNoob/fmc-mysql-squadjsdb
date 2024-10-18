7d104639e858f73c71db236443346cde685b1900
Misha Yudin <misha.yudin19@gmail.com>
2024-10-18 23:28:03 +0300

    Update trigger_dblogmatchesunignorelegit.sql
    Returned back winner check, better to update the winner

--- Triggers/trigger_dblogmatchesunignorelegit.sql
+++ Triggers/trigger_dblogmatchesunignorelegit.sql
@@ -4,7 +4,7 @@ before update on `dblog_matches`
 for each ROW
 BEGIN
     IF (NEW.`ignore` = 1 
-    --and NEW.winner is not null 
+    and NEW.winner is not null 
     and timestampdiff(minute, NEW.startTime, NEW.endTime) >= 15 and NEW.layerClassName not like '%_Seed%')
 	    THEN SET NEW.`ignore` = 0;
     END IF;
