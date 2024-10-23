9d6082bfb1b2e99fd524e3116680b9666735a871
Misha Yudin <misha.yudin19@gmail.com>
2024-10-23 16:04:11 +0300

    Update grafana_dashboardtotalkdr.sql
    Added wounds (a.k.a. knocks)

--- StoredProcedures/grafana_dashboardtotalkdr.sql
+++ StoredProcedures/grafana_dashboardtotalkdr.sql
@@ -2,7 +2,7 @@ drop procedure if exists `grafana_dashboardtotalkdr`;
 create procedure `grafana_dashboardtotalkdr`(fromVar varchar(255), toVar varchar(255), nicknameVar varchar(255))
 begin 
 	call sp_getstatsbytime(str_to_date(left(fromVar, 19),'%Y-%m-%dT%H:%i:%s'),str_to_date(left(toVar, 19),'%Y-%m-%dT%H:%i:%s'));
-	select sum(kills) as Kills, sum(deaths) as Deaths, sum(Revives) as Revives
+	select sum(kills) as Kills, sum(wounds) as Wounds, sum(deaths) as Deaths, sum(Revives) as Revives
 	from getstatsbytime 
 	where steamID = fn_getsteamidfromname(nicknameVar);
 	drop temporary table if exists getstatsbytime;
