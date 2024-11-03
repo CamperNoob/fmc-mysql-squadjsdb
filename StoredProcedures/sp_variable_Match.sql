709c3989d348570a3eba2df820c4cb2fa49d0585
Misha Yudin <misha.yudin19@gmail.com>
2024-11-03 23:17:58 +0200

    Update sp_variable_Match.sql
    Add order by

--- StoredProcedures/sp_variable_Match.sql
+++ StoredProcedures/sp_variable_Match.sql
@@ -15,14 +15,15 @@ BEGIN
         and matches.server != 3
         and exists (
             select 1 from steamidlistfromnamesjson as stjs
-            left join dblog_deaths as a on a.match = matches.id and a.teamkill != 1 and a.attacker = stjs.steamID
+            left join dblog_deaths as a on a.match = matches.id and a.teamkill <> 1 and a.attacker = stjs.steamID
             left join dblog_deaths as b on b.match = matches.id and b.victim = stjs.steamID
-            left join dblog_wounds as c on c.match = matches.id and c.teamkill != 1 and c.attacker = stjs.steamID
+            left join dblog_wounds as c on c.match = matches.id and c.teamkill <> 1 and c.attacker = stjs.steamID
             left join dblog_revives as d on d.match = matches.id and d.reviver = stjs.steamID
             where a.match is not null or b.match is not null or c.match is not null or d.match is not null)
         and matches.ignore = 0
         and matches.id in (select matchID from getmatchidsbymap)
-        and matches.server in (select serverID from serveridsfromnamesjson);
+        and matches.server in (select serverID from serveridsfromnamesjson)
+    order by matches.StartTime asc;
     drop temporary table if exists `getmatchidsbymap`;
     drop temporary table if exists `serveridsfromnamesjson`;
     drop temporary table if exists `steamidlistfromnamesjson`;
