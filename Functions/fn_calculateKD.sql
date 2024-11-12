c96be6ac3479a2628c0173d3755df8805b0d8192
Misha Yudin <misha.yudin19@gmail.com>
2024-11-12 22:30:43 +0200

    Remove deprecated, update for visual clarity

--- Functions/fn_calculateKD.sql
+++ Functions/fn_calculateKD.sql
@@ -3,13 +3,12 @@ CREATE FUNCTION `fn_calculateKD`(kills int, deaths int) RETURNS float
     DETERMINISTIC
 begin
 	declare result float;
-
-	IF kills = 0 THEN
+    IF kills = 0 THEN
 		SET result = 0;
-  ELSEIF deaths = 0 THEN
+    ELSEIF deaths = 0 THEN
 		SET result = kills;
-  ELSE
+    ELSE
       SET result = kills/deaths;
-  END IF;
+    END IF;
 	return result;
 end
