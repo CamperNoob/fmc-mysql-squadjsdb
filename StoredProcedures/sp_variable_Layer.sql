16a49eb5a23ee62196542ea08a046a5ae1f6c008
Misha Yudin <misha.yudin19@gmail.com>
2025-02-04 22:21:59 +0200

    Comment out advanced variable functionality to improve performance

--- StoredProcedures/sp_variable_Layer.sql
+++ StoredProcedures/sp_variable_Layer.sql
@@ -6,25 +6,26 @@ CREATE PROCEDURE `sp_variable_Layer`(
     serverVar TEXT,
     nicknameVar TEXT)
 BEGIN
-    call sp_serveridsfromnamesjson(serverVar);
-    call sp_steamidlistfromnamesjson(nicknameVar);
+--    call sp_serveridsfromnamesjson(serverVar);
+--    call sp_steamidlistfromnamesjson(nicknameVar);
 --    call sp_getmatchidsbytimestamps(matchVar);
     select maps.mapName as Layer from lookup_listofmaps as maps 
-    where maps.isActive = 1 and exists (
-        select 1 from dblog_matches as matches
-        where 
+    where maps.isActive = 1 --and exists (
+--        select 1 from dblog_matches as matches
+--        where 
 --            matches.id in (select matchId from getmatchidsbytimestamps) and
-            matches.server in (select serverID from serveridsfromnamesjson)
-            and matches.ignore = 0
-            and exists (
-                select 1 from steamidlistfromnamesjson as stjs
-                left join dblog_deaths as a on a.match = matches.id and a.teamkill != 1 and a.attacker = stjs.steamID
-                left join dblog_deaths as b on b.match = matches.id and b.victim = stjs.steamID
-                left join dblog_wounds as c on c.match = matches.id and c.teamkill != 1 and c.attacker = stjs.steamID
-                left join dblog_revives as d on d.match = matches.id and d.reviver = stjs.steamID
-                where a.match is not null or b.match is not null or c.match is not null or d.match is not null)
-            and matches.layerClassname like concat('%',maps.mapName,'%'));
-    drop temporary table if exists `serveridsfromnamesjson`;
-    drop temporary table if exists `steamidlistfromnamesjson`;
---    drop temporary table if exists `getmatchidsbytimestamps`;
+--            matches.server in (select serverID from serveridsfromnamesjson)
+--            and matches.ignore = 0
+--            and exists (
+--                select 1 from steamidlistfromnamesjson as stjs
+--                left join dblog_deaths as a on a.match = matches.id and a.teamkill != 1 and a.attacker = stjs.steamID
+--                left join dblog_deaths as b on b.match = matches.id and b.victim = stjs.steamID
+--                left join dblog_wounds as c on c.match = matches.id and c.teamkill != 1 and c.attacker = stjs.steamID
+--                left join dblog_revives as d on d.match = matches.id and d.reviver = stjs.steamID
+--                where a.match is not null or b.match is not null or c.match is not null or d.match is not null)
+--            and matches.layerClassname like concat('%',maps.mapName,'%'));
+--    drop temporary table if exists `serveridsfromnamesjson`;
+--    drop temporary table if exists `steamidlistfromnamesjson`;
+--    drop temporary table if exists `getmatchidsbytimestamps`
+;
 END
--- StoredProcedures/sp_variable_Server.sql
+++ StoredProcedures/sp_variable_Server.sql
@@ -4,30 +4,30 @@ CREATE PROCEDURE `sp_variable_Server`(
     toVar varchar(255),
     nicknameVar TEXT)
 BEGIN
-    call sp_steamidlistfromnamesjson(nicknameVar);
-    create temporary table steamidlist_copy1 as
-    select steamID from steamidlistfromnamesjson;
-    create temporary table steamidlist_copy2 as
-    select steamID from steamidlistfromnamesjson;
-    create temporary table steamidlist_copy3 as
-    select steamID from steamidlistfromnamesjson;
+--    call sp_steamidlistfromnamesjson(nicknameVar);
+--    create temporary table steamidlist_copy1 as
+--    select steamID from steamidlistfromnamesjson;
+--    create temporary table steamidlist_copy2 as
+--    select steamID from steamidlistfromnamesjson;
+--    create temporary table steamidlist_copy3 as
+--    select steamID from steamidlistfromnamesjson;
     select servers.name from dblog_servers as servers
     where servers.id != 3 and exists (
         select 1 from dblog_matches as matches
         where matches.ignore = 0
-            and (
-                    exists (
-                        select 1 from steamidlist_copy1 as stjs1
-                        join dblog_deaths as a on a.match = matches.id and a.teamkill != 1 and a.attacker = stjs1.steamID)
-                    or exists (
-                        select 1 from steamidlist_copy2 as stjs2
-                        join dblog_deaths as b on b.match = matches.id and b.teamkill != 1 and b.victim = stjs2.steamID)
-                    or exists (
-                        select 1 from steamidlist_copy3 as stjs3
-                        join dblog_wounds as c on c.match = matches.id and c.teamkill != 1 and c.attacker = stjs3.steamID))
+--            and (
+--                    exists (
+--                        select 1 from steamidlist_copy1 as stjs1
+--                        join dblog_deaths as a on a.match = matches.id and a.teamkill != 1 and a.attacker = stjs1.steamID)
+--                    or exists (
+--                        select 1 from steamidlist_copy2 as stjs2
+--                        join dblog_deaths as b on b.match = matches.id and b.teamkill != 1 and b.victim = stjs2.steamID)
+--                    or exists (
+--                        select 1 from steamidlist_copy3 as stjs3
+--                        join dblog_wounds as c on c.match = matches.id and c.teamkill != 1 and c.attacker = stjs3.steamID))
             and matches.server = servers.id);
-    drop temporary table if exists `steamidlistfromnamesjson`;
-    drop temporary table if exists `steamidlist_copy1`;
-    drop temporary table if exists `steamidlist_copy2`;
-    drop temporary table if exists `steamidlist_copy3`;
+--    drop temporary table if exists `steamidlistfromnamesjson`;
+--    drop temporary table if exists `steamidlist_copy1`;
+--    drop temporary table if exists `steamidlist_copy2`;
+--    drop temporary table if exists `steamidlist_copy3`;
 END
