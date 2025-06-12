beef737776db39483bd698bda81769184bad2963
Misha Yudin <misha.yudin19@gmail.com>
2025-06-12 14:20:01 +0300

    Added teamkills

--- StoredProcedures/grafana_dashboardkd.sql
+++ StoredProcedures/grafana_dashboardkd.sql
@@ -8,14 +8,14 @@ begin
 	call sp_steamidlistfromnamesjson(nicknameVar);
 	call sp_serveridsfromnamesjson(serverVar);
 	call sp_getmatchidsbytimestamps(matchTimestamps);
-	select playerName as Nickname, steamId as SteamID, sum(revives) as Revives, sum(wounds) as Wounds, sum(kills) as Kills, sum(deaths) as Deaths, sum(vehicles) as `Destroyed Vehicles`, fn_calculateKD(sum(kills), sum(deaths)) as `Kill\Deaths`, fn_calculateKD(sum(revives), sum(deaths)) as `Revive\Deaths`
+	select playerName as Nickname, steamId as SteamID, sum(revives) as Revives, sum(wounds) as Wounds, sum(kills) as Kills, sum(deaths) as Deaths, sum(teamkills) as Teamkills, sum(vehicles) as `Destroyed Vehicles`, fn_calculateKD(sum(kills), sum(deaths)) as `Kill\Deaths`, fn_calculateKD(sum(revives), sum(deaths)) as `Revive\Deaths`
 	from getstatsbytime 
 	where serverID in (select serverID from serveridsfromnamesjson) 
 		and matchId in (select matchId from getmatchidsbymap) 
 		and steamId in (select steamID from steamidlistfromnamesjson)
 		and matchId in (select matchId from getmatchidsbytimestamps)
 	group by playerName, steamId
-	order by (sum(vehicles)*5) + sum(revives) + sum(kills) + ((sum(wounds) - sum(kills))/10) - sum(deaths) desc;
+	order by (sum(vehicles)*5) + sum(revives) + sum(kills) + ((sum(wounds) - sum(kills))/10) - (sum(deaths) - sum(teamkills)) desc;
 	drop temporary table if exists getstatsbytime;
 	drop temporary table if exists getmatchidsbymap;
 	drop temporary table if exists steamidlistfromnamesjson;
--- StoredProcedures/sp_getstatsbytime.sql
+++ StoredProcedures/sp_getstatsbytime.sql
@@ -10,6 +10,7 @@ begin
 		deaths int,
 		wounds int,
 		revives int,
+		teamkills int,
 		vehicles int,
 		serverId int,
 		matchId int,
@@ -35,7 +36,6 @@ begin
 	group by server, `match`, victim
 	on duplicate key update deaths = values(deaths);
 
-
 	insert into getstatsbytime (steamID, wounds, serverID, matchID)
 	select attacker as steamID, count(victim) as wounds, server as serverID, `match` as matchID
 	from dblog_wounds
@@ -44,6 +44,15 @@ begin
 		and time between startDate and endDate
 	group by server, `match`, attacker
 	on duplicate key update wounds = values(wounds);
+
+	insert into getstatsbytime (steamID, teamkills, serverID, matchID)
+	select attacker as steamID, count(victim) as teamkills, server as serverID, `match` as matchID
+	from dblog_wounds
+	where teamkill = 1 and attacker != "00000000000000000" and attacker != victim
+		and `server` is not null and `match` is not null and attacker is not null and attacker not in (select a.steamId from dblog_players as a where a.ignore = 1)
+		and time between startDate and endDate
+	group by server, `match`, attacker
+	on duplicate key update teamkills = values(teamkills);
 
 	insert into getstatsbytime (steamID, revives, serverID, matchID)
 	select reviver as steamID, count(victim) as revives, server as serverID, `match` as matchID
@@ -85,6 +94,10 @@ begin
 	update getstatsbytime
 	set wounds = 0
 	where wounds is null;
+
+	update getstatsbytime
+	set teamkills = 0
+	where teamkills is null;
 
 	update getstatsbytime
 	set wounds = kills
